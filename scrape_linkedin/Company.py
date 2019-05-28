1d30d0237ad3254935f706ec73886335c6d396ef
Thomas Opsomer <thomasopsomer.enpc@gmail.com>
2019-05-28 19:32:26 +0200

    some change

--- scrape_linkedin/Company.py
+++ scrape_linkedin/Company.py
@@ -23,27 +23,22 @@ class Company(ResultsObject):
         """Return dict of the overview section of the Linkedin Page"""
 
         # Banner containing company Name + Location
-        banner = one_or_default(
-            self.overview_soup, '.org-top-card')
+        banner = one_or_default(self.overview_soup, ".org-top-card")
 
         # Main container with company overview info
-        container = one_or_default(
-            self.overview_soup, '.org-grid__core-rail--wide')
+        container = one_or_default(self.overview_soup, ".org-grid__core-rail--wide")
 
         overview = {}
-        overview['description'] = container.select_one(
-            'section > p').get_text().strip()
+        overview["description"] = container.select_one("section > p").get_text().strip()
 
-        metadata_keys = container.select('.org-page-details__definition-term')
-        print(metadata_keys)
-        metadata_keys = [x for x in metadata_keys if "Company size" not in x.get_text()]
-        print(metadata_keys)
-        metadata_values = container.select(
-            '.org-page-details__definition-text')
+        metadata_keys = container.select(".org-page-details__definition-term")
+        metadata_values = container.select(".org-page-details__definition-text")
         overview.update(
-            get_info(banner, {'name': '.org-top-card-summary__title'})) # A fix to the name selector
-        overview.update(
-            get_info(container, {'company_size': '.org-about-company-module__company-size-definition-text'})) # Manually added Company size
+            # get_info(banner, {'name': '.org-top-card-primary-content__title'}))
+            get_info(banner, {"name": ".org-top-card-summary__title"})
+        )
+        # overview.update(
+        #     get_info(container, {'company_size': '.org-about-company-module__company-size-definition-text'})) # Manually added Company size
 
         for key, val in zip(metadata_keys, metadata_values):
             dict_key = key.get_text().strip().lower().replace(" ", "_")
@@ -55,23 +50,23 @@ class Company(ResultsObject):
         # all_employees_links = all_or_default(
         #     banner, '.org-company-employees-snackbar__details-highlight')
         all_employees_links = all_or_default(
-            banner, '.mt2 > a > span') # A fix to locate "See all ### employees on LinkedIn"
-            # banner, 'a[data-control-name="topcard_see_all_employees"]')
+            banner, '.mt2 > a > span'   # A fix to locate "See all ### employees on LinkedIn"
+            # banner, 'a[data-control-name="topcard_see_all_employees"]'
+        )
 
         if all_employees_links:
             all_employees_text = all_employees_links[-1].text
         else:
-            all_employees_text = ''
+            all_employees_text = ""
 
-        match = re.search(r'((\d+?,?)+)', all_employees_text)
+        match = re.search(r"((\d+?,?)+)", all_employees_text)
         if match:
-            overview['num_employees'] = int(match.group(1).replace(',', ''))
+            overview["num_employees"] = int(match.group(1).replace(",", ""))
         else:
-            overview['num_employees'] = None
+            overview["num_employees"] = None
 
-        logo_image_tag = one_or_default(
-            banner, '.org-top-card-primary-content__logo')
-        overview['image'] = logo_image_tag['src'] if logo_image_tag else ''
+        logo_image_tag = one_or_default(banner, ".org-top-card-primary-content__logo")
+        overview["image"] = logo_image_tag["src"] if logo_image_tag else ""
 
         return overview
 
